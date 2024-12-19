f7a6c328da67915205d39c614ec498e16e368f3c
hamza <hyusufah@outlook.com>
2024-12-19 15:05:01 +0000

    import necessary libraries for Data Analysis and Linear Regression

---
+++ Scripts/data analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
+
+
+
