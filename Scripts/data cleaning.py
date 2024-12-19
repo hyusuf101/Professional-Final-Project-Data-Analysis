2bd8f5ab6faaeb1fb84e532ca5187a50a7854000
hamza <hyusufah@outlook.com>
2024-12-19 14:51:34 +0000

    Create Data Cleaning script

---
+++ Scripts/data cleaning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def clean_data(df):
+    # Rename the first two columns
+    df.rename(columns={df.columns[0]: 'Crime Category', df.columns[1]: 'Subcategory'}, inplace=True)
+    
+    # Format the rest of the columns to be dates
+    for col in df.columns[2:]:
+        df.rename(columns={col: pd.to_datetime(col, format='%Y%m').strftime('%Y-%m')}, inplace=True)
+    
+  
+    df = df.dropna()  # Drop rows with missing values
+    
+    return df
