69d5947d4d745bba6f7bca3d4b96231a2a9e0182
hamza <hyusufah@outlook.com>
2024-12-19 16:22:52 +0000

    modified data cleaning file

--- Scripts/data cleaning.py
+++ Scripts/data cleaning.py
@@ -5,7 +5,7 @@ def clean_data(df):
     df.rename(columns={df.columns[0]: 'Crime Category', df.columns[1]: 'Subcategory'}, inplace=True)
     
     # Format the rest of the columns to be dates
-    for col in df.columns[2:]:
+    for col in df.columns[3:]:
         df.rename(columns={col: pd.to_datetime(col, format='%Y%m').strftime('%Y-%m')}, inplace=True)
     
   
