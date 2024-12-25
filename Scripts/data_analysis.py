bdfdbd19f85bda35d3f609b4a0961e42eba707fb
hamza <hyusufah@outlook.com>
2024-12-25 01:36:13 +0000

    Added population density values from GOV website needed for data analysis

--- Scripts/data_analysis.py
+++ Scripts/data_analysis.py
@@ -25,3 +25,21 @@ crime = data_loading.load_data(data_path)
 # Clean data
 clean_data = data_cleaning.clean_data(crime)
 
+#Added population density values from GOV census
+population_density = pd.DataFrame({
+    'Borough': [
+        'City of London', 'Westminster', 'Camden', 'Islington', 'Hackney', 
+        'Tower Hamlets', 'Southwark', 'Lambeth', 'Wandsworth', 'Hammersmith and Fulham', 
+        'Brent', 'Ealing', 'Haringey', 'Newham', 'Waltham Forest', 'Lewisham', 
+        'Greenwich', 'Bexley', 'Bromley', 'Croydon', 'Sutton', 'Merton', 
+        'Kingston upon Thames', 'Richmond upon Thames', 'Havering', 'Barking and Dagenham', 
+        'Redbridge', 'Hillingdon', 'Harrow', 'Enfield', 'Barnet', 'Hounslow', 'Kensington and Chelsea'
+    ],
+    'Density': [
+        4790, 11200, 12800, 13300, 16200, 15800, 9800, 10700, 10900, 
+        11500, 8900, 8700, 10600, 10500, 9600, 9300, 8800, 6500, 
+        6300, 7200, 5800, 6700, 5900, 5600, 5400, 6800, 6200, 5500, 
+        5700, 5600, 5800, 6000, 14800 
+    ]
+})
+
