ab75c9d43009f097bffe6ba110496b420ef19cff
hamza <hyusufah@outlook.com>
2024-12-25 01:22:40 +0000

    import data loading and cleaning modules and read in CSV file

--- Scripts/data analysis.py
+++ Scripts/data analysis.py
@@ -8,3 +8,19 @@ from scipy import stats
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LinearRegression
 
+import data loading
+import data cleaning
+import os #for directory filepath
+
+
+# Get the root directory of the project
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the data file
+data_path = os.path.join(root_dir, 'data', 'MPS Borough Level Crime (most recent 24 months).csv')
+
+# Load data
+crime = loading.load_data(data_path)
+
+
+
