56a030e36c3edebe768ac43d11068fb2e838f727
hamza <hyusufah@outlook.com>
2024-12-25 00:59:09 +0000

    add all libraries needed for analysis

--- Scripts/data analysis.py
+++ Scripts/data analysis.py
@@ -3,9 +3,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LinearRegression
-
-
 
